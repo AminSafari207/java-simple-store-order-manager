883155bd6b5fcb3945c0a35ad570112ba6cc5ae0
AminSafari207 <safarin207368@gmail.com>
2025-08-16 09:58:29 +0330

    Improve and debug BaseRepository interface

--- src/main/java/repository/BaseRepository.java
+++ src/main/java/repository/BaseRepository.java
@@ -1,11 +1,12 @@
 package repository;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface BaseRepository<T> {
+public interface BaseRepository<T, ID> {
     void create(T t);
     List<T> findAll();
-    T findById();
+    Optional<T> findById(ID id);
     void update(T t);
     void delete(T t);
 }
