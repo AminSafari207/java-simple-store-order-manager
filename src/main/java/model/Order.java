f6d273c8fb200d630fb286a13d8fcbb3d4ce6431
AminSafari207 <safarin207368@gmail.com>
2025-08-16 09:30:21 +0330

    Implement Order model class

---
+++ src/main/java/model/Order.java
@@ -0,0 +1,60 @@
+package model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String customerName;
+
+    private LocalDate orderDate;
+
+    private Double totalAmount;
+
+    public Order(Long id, String customerName, LocalDate orderDate, Double totalAmount) {
+        this.id = id;
+        this.customerName = customerName;
+        this.orderDate = orderDate;
+        this.totalAmount = totalAmount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public Double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(Double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+}
