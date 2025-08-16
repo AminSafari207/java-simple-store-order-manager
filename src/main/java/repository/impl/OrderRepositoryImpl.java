a1bec5299493466b2f59b9f5bf43d39f8f6cab89
AminSafari207 <safarin207368@gmail.com>
2025-08-16 10:56:01 +0330

    Implement findAll method for order repo impl

---
+++ src/main/java/repository/impl/OrderRepositoryImpl.java
@@ -0,0 +1,59 @@
+package repository.impl;
+
+import exception.RepositoryException;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceException;
+import model.Order;
+import org.hibernate.QueryException;
+import repository.OrderRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class OrderRepositoryImpl implements OrderRepository {
+    private final EntityManager em;
+
+    public OrderRepositoryImpl(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public void create(Order order) {
+        try {
+            em.persist(order);
+        } catch (PersistenceException e) {
+            throw new RepositoryException("Persisting order has been failed.", e);
+        }
+    }
+
+    @Override
+    public List<Order> findAll() {
+        String hqlQuery = "Select o from Order o";
+
+        try {
+            return em.createQuery(hqlQuery, Order.class).getResultList();
+        } catch (PersistenceException e) {
+            throw new RepositoryException("Finding all orders has been failed.", e);
+        }
+    }
+
+    @Override
+    public Optional<Order> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Order> findByCustomerName(String name) {
+        return List.of();
+    }
+
+    @Override
+    public void update(Order order) {
+
+    }
+
+    @Override
+    public void delete(Order order) {
+
+    }
+}
