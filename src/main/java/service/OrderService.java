1e1bcbb9a860d6fd8c7fecf2cdecf53cace189c5
AminSafari207 <safarin207368@gmail.com>
2025-08-17 12:16:55 +0330

    Implement transaction callback executer for order service

--- src/main/java/service/OrderService.java
+++ src/main/java/service/OrderService.java
@@ -8,6 +8,7 @@ import repository.OrderRepository;
 import repository.impl.OrderRepositoryImpl;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class OrderService {
     private final EntityManagerFactory emf;
@@ -27,6 +28,40 @@ public class OrderService {
             repository.create(order);
 
             tx.commit();
+        } catch (Exception e) {
+            if (tx.isActive()) tx.rollback();
+            throw new RuntimeException("Transaction failed", e);
+        } finally {
+            em.close();
+        }
+    }
+
+    public void executeTransaction(Consumer<EntityManager> consumer) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            consumer.accept(em);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx.isActive()) tx.rollback();
+            throw new RuntimeException("Transaction failed", e);
+        } finally {
+            em.close();
+        }
+    }
+
+    public <T> T executeTransaction(Function<EntityManager, T> function) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            T res = function.apply(em);
+            tx.commit();
+
+            return res;
         } catch (Exception e) {
             if (tx.isActive()) tx.rollback();
             throw new RuntimeException("Transaction failed", e);
